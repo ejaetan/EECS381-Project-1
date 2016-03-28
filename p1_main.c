25a524cbd88a1764fb76114b6ae0de7da8d3c991
Yi Jie Tan <ejae.tan@gmail.com>
2016-03-28 08:30:33 -0700

    Tested ai command

--- p1_main.c
+++ p1_main.c
@@ -17,7 +17,7 @@
 
 /* Function Prototype */
 int compare_Rooms_number(int num1, int num2);
-
+void skip_type_ahead(void);
 void add_individual(struct Ordered_container* People);
 
 
@@ -31,52 +31,64 @@ int main(void) {
     assert(Rooms);
     assert(People);
     
-    printf("\nEnter command: ");
-    
-    int var1 = scanf(" %c", &char1);
-    int var2 = scanf(" %c", &char2);
-    
-    assert((var1 == 1)&& (var2 == 1));
-    
-    switch (char1) {
-        case 'p':
-            switch (char2) {
-                case 'i':
-                    break;
-                    
-                default:
-                    break;
-            }
-            break;
-        case 'a':
-            add_individual(People);
-            break;
-        case 'r':
-            break;
-        case 'd':
-            break;
-        case 's':
-            break;
-        case 'l':
-            break;
-        case 'q':
-            switch (char2) {
-                case 'q':
-                    OC_apply(Rooms, (OC_apply_fp_t) destroy_Room);
-                    printf("All meetings deleted\n");
-                    OC_destroy_container(Rooms);
-                    printf("All rooms deleted\n");
-                    OC_destroy_container(People);
-                    printf("All persons deleted\n");
-                    printf("Done\n");
-                    break;
-                    
-                default:
-                    break;
-            }
-            break;
-        default:
-            break;
+    while (1) {
+        printf("\nEnter command: ");
+        
+        int var1 = scanf(" %c", &char1);
+        int var2 = scanf(" %c", &char2);
+        
+        assert((var1 == 1)&& (var2 == 1));
+        
+        switch (char1) {
+            case 'p':
+                switch (char2) {
+                    case 'i':
+                        break;
+                        
+                    default:
+                        break;
+                }
+                break;
+            case 'a':
+                switch (char2) {
+                    case 'i':
+                        add_individual(People);
+                        break;
+                        
+                    default:
+                        break;
+                }
+                break;
+            case 'r':
+                break;
+            case 'd':
+                break;
+            case 's':
+                break;
+            case 'l':
+                break;
+            case 'q':
+                switch (char2) {
+                    case 'q':
+                        OC_apply(Rooms, (OC_apply_fp_t) destroy_Room);
+                        printf("All meetings deleted\n");
+                        OC_destroy_container(Rooms);
+                        printf("All rooms deleted\n");
+                        OC_destroy_container(People);
+                        printf("All persons deleted\n");
+                        printf("Done\n");
+                        return 0;
+                        break;
+                        
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        skip_type_ahead();
+        
     }
     
     
@@ -89,7 +101,24 @@ int compare_Rooms_number(int num1, int num2) {
     return num1 - num2;
 }
 
-void add_individual(struct Ordered_container* People) {
-    
+void skip_type_ahead(void) {
+    scanf("%*[^\n]");
 }
+void add_individual(struct Ordered_container* People) {
+    char *firstname[MAX_CHAR], *lastname[MAX_CHAR], *phoneno[MAX_CHAR];
+    int scan_input = scanf("%63s %63s %63s", firstname, lastname, phoneno);
+    assert(scan_input == 3);
+    
+    void* find_people_item_ptr = OC_find_item_arg(People, lastname, (OC_find_item_arg_fp_t) get_Person_lastname);
+    
+    if (find_people_item_ptr) {
+        printf("There is already a person with this last name!\n");
+    } else {
+        struct Person* new_Person = create_Person((const char*) firstname, (const char*) lastname, (const char*) phoneno);
+        OC_insert(People, new_Person);
+        printf("Person %s added\n", (const char*) lastname);
+    }
+}
+
+
 
